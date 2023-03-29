d4d27814357b8cd44128e90bf7326fae3d0fd14b
Artur Pilarek <artur.pilarek@gmail.com>
2023-03-29 08:44:27 +0200

    Added api route to create and read comments based on given eventId

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,48 @@
+import clientPromise from "../../../lib/mongodb"
+
+export default async (req, res) => {
+  const { eventId } = req.query
+
+  try {
+    const client = await clientPromise
+    const db = client.db("newsletter-comments")
+
+    const newsletter = await db.collection("comments").find({}).toArray()
+
+    if (req.method === "POST") {
+      const { email, name, comment } = req.body
+
+      if (
+        !email ||
+        !email.includes("@") ||
+        !name ||
+        name.trim() === "" ||
+        !comment ||
+        comment.trim() === ""
+      ) {
+        res.status(422).json({ message: "Invalid input." })
+        return
+      }
+
+      const newComment = await db.collection("comments").insertOne({
+        email,
+        name,
+        comment,
+        eventId,
+        dateAdded: new Date().toISOString(),
+      })
+
+      res.json(newComment)
+    } else if (req.method === "GET") {
+        
+      const comments = await db
+        .collection("comments")
+        .find({ eventId })
+        .toArray()
+
+      res.json(comments)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
